0819bcdba235e52165ce2977b8ee3eddc50da964
David Serrano <75829857+serranoio@users.noreply.github.com>
2025-05-25 17:28:29 -0700

    changes

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
 import { BookeraModule, DEFAULT_VERSION } from '@serranolabs.io/shared/module';
-import { $ModuleElementName } from './{module_element_kc}';
+import { $ModuleElementName, elementName } from './{module_element_kc}';
 import { Tab } from '@serranolabs.io/shared/tab';
 import { genShortID } from '@serranolabs.io/shared/util';
 
@@ -13,11 +13,10 @@ export const $moduleElementNameModule = new BookeraModule(
     '{module_name_hr}',
     '{tab.icon}',
     '',
-    '{module_element_kc}',
-    '{shouldShowLeftSide}',
-    [`{renderModes}`]
+    elementName,
+    '{shouldShowLeftSide}'
   ).removeTab(),
   genShortID(10),
-  [],
+  [`{renderModes}`],
   $ModuleElementName
 );
