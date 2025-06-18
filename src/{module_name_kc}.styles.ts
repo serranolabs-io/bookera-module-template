750a52cdef856b1fb54ddc9fe0e9162383857d60
David Serrano <75829857+serranoio@users.noreply.github.com>
2025-06-18 15:24:25 -0700

    fixed up template

--- src/{module_name_kc}.styles.ts
+++ src/{module_name_kc}.styles.ts
@@ -1,2 +1,27 @@
 import { css } from 'lit';
-export default css``;
+export default css`
+  /* 
+SIDE PANEL
+*/
+  /* 
+END SIDE PANEL
+*/
+  /* 
+PANEL
+*/
+  /* 
+END PANEL
+*/
+  /* 
+MODULE
+*/
+  /* 
+END MODULE
+*/
+  /* 
+DAEMON
+*/
+  /* 
+END DAEMON
+*/
+`;
